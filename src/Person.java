b60549e1a74b309b53e1039fb4594b56136ce254
andyBzg <andr.bzg@gmail.com>
2022-12-26 14:23:59 +0100

    Getter and Setter added Class converted to abstract class New abstract method

--- src/Person.java
+++ src/Person.java
@@ -1,38 +1,79 @@
-public class Person {
+public abstract class Person {
 
     private String name;
     private int age;
     private int height;
-    private double weight;
-    private int money;
+    private int weight;
 
 
-    public Person(String name, int age, int height, double weight) {
+    public Person(String name, int age, int height, int weight) {
         this.name = name;
         this.age = age;
         this.height = height;
         this.weight = weight;
     }
 
-    public Person() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 1 || age > 110) {
+            System.err.println("Incorrect age");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        if (height < 0) {
+            System.err.println("Incorrect height");
+        } else {
+            this.height = height;
+        }
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        if (weight < 0) {
+            System.err.println("Incorrect weight");
+        } else {
+            this.weight = weight;
+        }
+    }
+
+
+    public abstract void die();
+
 
     public void info() {
         System.out.println(name);
         System.out.println(age);
         System.out.println(height);
         System.out.println(weight);
-        System.out.println(money);
     }
 
     public void goToWork() {
-        if (age < 18 || age > 70){
+        if (age < 18 || age > 70) {
             System.out.println("отдыхаю дома");
-        }
-        else System.out.println("работаю");
+        } else System.out.println("работаю");
     }
 
-    public int getMoney() {
-        return money;
-    }
 
 }
