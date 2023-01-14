f6a26729ac1012f05c23158257538f7fe30876bd
andyBzg <andr.bzg@gmail.com>
2023-01-14 09:29:54 +0100

    Initial commit

---
+++ src/Month.java
@@ -0,0 +1,25 @@
+public enum Month {
+
+    JANUARY(1.1),
+    FEBRUARY(0),
+    MARCH(0),
+    APRIL(0),
+    MAY(0),
+    JUNE(0),
+    JULY(0),
+    AUGUST(0),
+    SEPTEMBER(0),
+    OCTOBER(0),
+    NOVEMBER(2),
+    DECEMBER(10);
+
+    private double number; // приватное поле
+
+    Month(double number) { // конструктор
+        this.number = number;
+    }
+
+    public double getNumber() { // геттер, сеттер создавать не надо
+        return number;
+    }
+}
