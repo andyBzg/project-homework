c78d8b7b6f8cdfddba3275007571d2751e17bac3
andyBzg <andr.bzg@gmail.com>
2023-01-15 10:40:28 +0100

    Initial commit

---
+++ src/Company.java
@@ -0,0 +1,42 @@
+import java.util.Objects;
+
+public class Company {
+
+    private String name;
+
+
+    public Company(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Company company = (Company) o;
+
+        return Objects.equals(name, company.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
