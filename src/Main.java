f54811bc08f3edf0baccef9795998c9c2096e94f
andyBzg <andr.bzg@gmail.com>
2023-01-22 19:46:40 +0100

    Tests

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,39 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
         Worker worker = new Worker("Vasya", 63, 170, 86);
         worker.setMinSalary(1551);
-        worker.setMaxSalary(2357);
-        System.out.println(worker.calculatePension());
+        worker.setMaxSalary(2357.22);
 
+        List<String> list = new LinkedList<>();
+        list.add("Tanya");
+        list.add("Kolya");
+
+        worker.setChildren(list);
+        System.out.println(worker.calculatePension());
+        System.out.println();
+
+        Pensioner pensioner = new Pensioner("Ivan Ivanovich", 98, 170, 86, 800.5);
+        pensioner.setChildren(list);
+        pensioner.die();
+        System.out.println();
 
         PensionFund stateFund = new PensionFund("Government", FundType.STATE, "01.01.23");
         PensionFund nonStateFund = new PensionFund("Private", FundType.NON_STATE, "02.02.23");
         PensionFund fakeFund = new PensionFund("MMM", FundType.SCAMMERS, "03.03.23");
         System.out.println();
 
-        double firstTest = stateFund.calculatePensionPayment(45, 1551, 2357);
+        double firstTest = stateFund.calculatePensionPayment(45, 1551, 2357.22);
         System.out.println(firstTest);
 
-        double secondTest = nonStateFund.calculatePensionPayment(45, 1551, 2357);
+        double secondTest = nonStateFund.calculatePensionPayment(45, 1551, 2357.22);
         System.out.println(secondTest);
 
-        double thirdTest = fakeFund.calculatePensionPayment(45, 1551, 2357);
+        double thirdTest = fakeFund.calculatePensionPayment(45, 1551, 2357.22);
         System.out.println(thirdTest);
     }
 }
