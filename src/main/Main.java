c60e4ad7ea4e5d816b6fe7f51ca91780ccb7d7bb
andyBzg <andr.bzg@gmail.com>
2023-01-27 18:02:20 +0100

    commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         Worker firstWorker = new Worker("Ivan");
-        List<Person> firsListOfChildren = new ArrayList<>();
+        List<Child> firsListOfChildren = new ArrayList<>();
         firsListOfChildren.add(new Child("Kolya"));
         firsListOfChildren.add(new Child("Tanya"));
         firsListOfChildren.add(new Child("Masha"));
@@ -17,7 +17,7 @@ public class Main {
 
 
         Worker secondWorker = new Worker("Petya");
-        List<Person> secondListOfChildren = new ArrayList<>();
+        List<Child> secondListOfChildren = new ArrayList<>();
         secondListOfChildren.add(new Child("Vitaly"));
         secondListOfChildren.add(new Child("Sveta"));
         secondListOfChildren.add(new Child("Misha"));
@@ -26,11 +26,8 @@ public class Main {
 
 
         Worker thirdWorker = new Worker("Vasya");
-        List<Company> companies = new ArrayList<>();
-        companies.add(new Company("Google"));
-        companies.add(new Company("Apple"));
-        companies.add(new Company("Tesla"));
-        thirdWorker.setJob(companies);
+        List<Company> companies = List.of(new Company("Apple"), new Company("Google"));
+        thirdWorker.setPreviousJob(companies);
 
 
         firstWorker.showChildren();
