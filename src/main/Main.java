edf18b4ac79c537cf05be9af47e6935ca2bf8d82
andyBzg <andr.bzg@gmail.com>
2023-03-30 21:44:29 +0200

    Add check for null

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,25 @@
 package main;
 
 import classes.*;
+import enums.FundType;
 
 import java.io.IOException;
 import java.util.List;
 
 
 public class Main {
+
+    private static final int MAX_NAME_LENGTH = 15;
+
     public static void main(String[] args) throws IOException {
 
         List<PensionFund> pensionFunds = FundGenerator.generate();
 
         pensionFunds.stream()
+                .filter(f -> f.getFundType() != null)
                 .filter(f -> f.getFundType().equals(FundType.STATE))
-                .filter(f -> f.getName().length() < 15)
+                .filter(f -> f.getName() != null)
+                .filter(f -> f.getName().length() < MAX_NAME_LENGTH)
                 .forEach(System.out::println);
 
     }
