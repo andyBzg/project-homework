86f23373cf5eb3e35863af26eadb7d2c83a75ff5
andyBzg <andr.bzg@gmail.com>
2023-03-30 21:47:28 +0200

    Add try-with-resources block

--- src/classes/FundGenerator.java
+++ src/classes/FundGenerator.java
@@ -11,23 +11,26 @@ import java.util.Random;
 
 public class FundGenerator {
 
-    public static List<PensionFund> generate() throws IOException {
+    public static List<PensionFund> generate() {
 
         String path = "resources/fund.txt";
 
         Random random = new Random();
         List<PensionFund> funds = new ArrayList<>();
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
 
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            funds.add(new PensionFund(
-                    line,
-                    FundType.getRandomType(),
-                    String.valueOf(random.nextInt(1900, 2000))
-            ));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                funds.add(new PensionFund(
+                        line,
+                        FundType.getRandomType(),
+                        String.valueOf(random.nextInt(1900, 2000))
+                ));
+            }
         }
-        bufferedReader.close();
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return funds;
     }
