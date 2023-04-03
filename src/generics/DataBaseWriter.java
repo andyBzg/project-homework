f18ada02f87e304c3bdc2adedb0bcd673bcb90e1
andyBzg <andr.bzg@gmail.com>
2023-04-03 12:53:34 +0200

    commit

--- src/generics/DataBaseWriter.java
+++ src/generics/DataBaseWriter.java
@@ -1,8 +1,5 @@
 package generics;
 
-import classes.Person;
-import classes.Worker;
-
 public class DataBaseWriter<T> {
 
     private String database;
@@ -18,13 +15,4 @@ public class DataBaseWriter<T> {
         System.out.printf("данные в бд записаны, тип данных = %s", value.getClass());
     }
 
-
-    public static void main(String[] args) {
-
-        Worker worker = new Worker("Vasya");
-
-        DataBaseWriter<Person> database = new DataBaseWriter<>("DB", worker);
-        database.save();
-
-    }
 }
