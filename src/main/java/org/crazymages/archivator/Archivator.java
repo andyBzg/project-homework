ee8243f30a377e2f1e4f974833e94d5112e98226
andyBzg <andr.bzg@gmail.com>
2023-04-15 18:41:53 +0200

    commit

--- src/main/java/org/crazymages/archivator/Archivator.java
+++ src/main/java/org/crazymages/archivator/Archivator.java
@@ -2,7 +2,7 @@ package org.crazymages.archivator;
 
 public abstract class Archivator {
 
-    public abstract void archive (String fileName);
+    public abstract void archive(String fileName);
 
-    public abstract void dearchive (String fileName);
+    public abstract void dearchive(String fileName);
 }
